6d4e7aa6cdc6eb5233f6fe2035b2ad38b414fea3
Matas Griškus <135244071+mgriskus@users.noreply.github.com>
2023-12-03 14:22:01 +0200

    Add basic math functions in the JS

---
+++ script.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(num1, operator, num2) {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      return num1 / num2;
+  }
+  return NaN;
+}
+
+let num1
+  , operator
+  , num2;
+
