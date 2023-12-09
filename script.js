f2046dcf3db215fbc70756b18a24fd683269544f
Matas Griškus <135244071+mgriskus@users.noreply.github.com>
2023-12-09 10:44:48 +0200

    Add basic calculator

--- script.js
+++ script.js
@@ -1,34 +1,96 @@
-function add(a, b) {
-  return a + b;
+const display = document.querySelector(".display");
+const buttonContainer = document.querySelector(".button-container");
+
+let num1 = undefined,
+  operator = "",
+  num2 = undefined,
+  result = undefined,
+  isPointInNum = false;
+
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function subtract(a, b) {
-  return a - b;
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-function multiply(a, b) {
-  return a * b;
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
-function divide(a, b) {
-  return a / b;
+function divide(num1, num2) {
+  return num1 / num2;
 }
 
 function operate(num1, operator, num2) {
   switch (operator) {
     case "+":
-      return num1 + num2;
+      return add(num1, num2);
     case "-":
-      return num1 - num2;
+      return subtract(num1, num2);
     case "*":
-      return num1 * num2;
+      return multiply(num1, num2);
     case "/":
-      return num1 / num2;
+      if (num2 == 0) return "Division by 0 is impossible";
+      return divide(num1, num2);
   }
-  return NaN;
 }
 
-let num1
-  , operator
-  , num2;
+function partialReset() {
+  num1 = undefined;
+  operator = "";
+  num2 = undefined;
+  display.textContent = "";
+}
 
+buttonContainer.addEventListener("mouseup", (e) => {
+  let buttonClicked = e.target.textContent;
+  if (buttonClicked == "AC") {
+    partialReset();
+    result = undefined;
+  } else if (
+    (buttonClicked == "+" || buttonClicked == "-") &&
+    !display.textContent
+  ) {
+    display.textContent += buttonClicked;
+  } else if (buttonClicked == ".") {
+    if (!isPointInNum) {
+      display.textContent += ".";
+      isPointInNum = true;
+    }
+  } else if (!isNaN(buttonClicked)) {
+    if (!isNaN(result) || isNaN(display.textContent + 1)) {
+      display.textContent = "";
+      num1 = undefined;
+    }
+    display.textContent += buttonClicked;
+  } else if (display.textContent) {
+    if (num1 !== undefined) {
+      num2 = +display.textContent;
+      isPointInNum = false;
+      result = operate(num1, operator, num2);
+      if (result != "Division by 0 is impossible") {
+        result = Math.round(result * 100) / 100;
+      } else {
+        display.textContent = "Division by 0 is impossible";
+      }
+      if (buttonClicked == "=") {
+        partialReset();
+        display.textContent = result;
+      } else {
+        current = "";
+        num1 = result;
+      }
+    } else {
+      result = undefined;
+      if (buttonClicked == "=" || isNaN(display.textContent)) {
+        return;
+      }
+      num1 = +display.textContent;
+      isPointInNum = false;
+      operator = buttonClicked;
+      display.textContent = "";
+    }
+  }
+});
